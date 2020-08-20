857f78949e610542227be9ad212300e4906f7d00
B M <adjunctfuentes@gmail.com>
2020-08-19 21:11:59 -0500

    Final Code for PyPoll

--- PyPoll/Analysis/main.py
+++ PyPoll/Analysis/main.py
@@ -15,84 +15,101 @@ with open(csvpath, 'r') as csvfile:
     csv_header = next(csvreader)
     print(f"CSV Header : {csv_header}")
 
-    #vote counter start
-    vote_count = 0
-
 #may need list of data
-
-votes = []
-candidate = []
-
-
-#function that will review the election data
-def print_electionresults(election_data):
-    #assign values to variables
-     votes = int(election_data[0])
-     candidate = str(election_data[2])
-
-     #The total number of votes cast
-     total_votes = len(votes)
-
-     #A complete list of candidates who received votes by total amount of voter ids
-    candidate_list =[]
-    for person in candidate:
-        if person not in candidate_list:
-            candidate_list.append(person)
-            
-        print([candidate_list])
-individual_votes = 1
-
-    #The total number of votes each candidate won
-
-
-    #the percentage of votes each candidate won
-        percent = round(candidate_list)
-
-   
-
-
-  #The winner of the election based on popular vote.
-        winner = max()
-
-
- #print out the results of the information
-    print(Election Results)
-    print('-'*15)
-    print(f'Total Votes: {total_votes}')
-    print('-'*15)
-    print(f' ')
-    print(f'')
-    print(f'')
-    print('-'*15)
-    print(f'Winner: {winner}')
-    print('-'*15)
-
-
+    vote_count = 0
+    votes_list = []
+    candidate_list = []
+    candidate_votes = []
+    
 #loop through the data
     for row in csvreader:
-        print()
-        print()
+        #print(row)
+        #The total number of votes cast and add to votes_list
+        votes = int(row[0])
+        votes_list.append(votes)
+        
+       #A complete list of candidates who received votes by total amount of voter ids
+       #in loop print each candidate name, once the candidate name appears start the counter at 1 and add one every time a vote counts for that candidate
+       
+        candidate = str(row[2])
+        if candidate not in candidate_list:
+            candidate_list.append(candidate)
+            candidate_votes.append(0)
+        
+        #alternative to index below but less efficient because separate loop
+        # for i in range(len(candidate_list)):
+        #     if candidate == candidate_list[i]:
+        #         candidate_votes[i]+=1
+
+        #index assigned to candidate and with each candidate count each vote
+        i = candidate_list.index(candidate)
+        candidate_votes[i]+=1
+
+total_votes = len(votes_list)
+#print(total_votes)
+#print(candidate_list)
+#print(candidate_votes)     
+
+#the percentage of votes each candidate won which and print each
+percent = []
+#separate loop to run through candidates and votes and then add calculation to list
+#round(rounds to integer, 3 is decimal places)
+for vote in candidate_votes:
+    percent.append(round(vote/total_votes *100,3))
+
+#print(percent)
+
+#The winner of the election based on popular vote. using the index value for each candidate based on votes get the max(votes)
+winner_index = candidate_votes.index(max(candidate_votes))
+#once the winner index is determined get the name out of candidate_list
+winner = candidate_list[winner_index]
+#print(winner)
+
+#detail that results will go in text file 
+results_output = "electionoutput.txt"
+# print(results_output)
+
+#write output data in new txt file
+results_file = open(results_output, 'w') 
 
 
-#possibly zip the data before output
+#print out the results of the information by assigning output_string to each data point, printing to terminal, then adding to txt file with \n new line after each one.
+output_string = "Election Results"
+print(output_string)
+results_file.write(output_string+"\n") 
 
-results_output = os.pth.join('Electionoutput.csv')
+output_string= ('-'*15)
+print(output_string)
+results_file.write(output_string+"\n")  
 
-#write output data in new csv
-with open(stats_output, 'w') as csvfile:
-    
-    #initalize csv.writer
-    csvwriter = csv.writer(csvfile, delimiter=',')
+output_string= (f'Total Votes: {total_votes}')
+print(output_string)
+results_file.write(output_string+"\n") 
 
-    #write the results on the new csv
-    csvwriter.writerow(Election Results)
-    csvwriter.writerow('-'*15)
-    csvwriter.writerow(f'Total Votes: {total_votes}')
-    csvwriter.writerow('-'*15)
-    csvwriter.writerow(f' ')
-    csvwriter.writerow(f'')
-    csvwriter.writerow(f'')
-    csvwriter.writerow('-'*15)
-    csvwriter.writerow(f'Winner: {winner}')
-    csvwriter.writerow('-'*15)
-    csvwriter.writerow(
+output_string= ('-'*15)
+print(output_string)
+results_file.write(output_string+"\n") 
+
+#loop through each range of indexes (i) for all candidates in entire (len) candidate list
+for i in range(len(candidate_list)):
+    output_string= (f'{candidate_list[i]} : {percent[i]}% ({candidate_votes[i]})')
+    print(output_string)
+    results_file.write(output_string+"\n") 
+
+output_string= ('-'*15)
+print(output_string)
+results_file.write(output_string+"\n") 
+
+output_string= (f'Winner: {winner}')
+print(output_string)
+results_file.write(output_string+"\n") 
+
+output_string= ('-'*15)
+print(output_string)
+results_file.write(output_string) 
+
+#flushes information from ram since writing to txt file
+results_file.flush()
+#closes the file after
+results_file.close()
+
